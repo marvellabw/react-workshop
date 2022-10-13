4b3eb5a8e941938007c1e386c014c90cd7d7b831
Vella BW <marvellaaabw@gmail.com>
2022-10-13 16:31:17 +1300

    add fetch mock

--- src/Login.test.js
+++ src/Login.test.js
@@ -3,10 +3,38 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import 'whatwg-fetch';
 import Login from './Login';
+import checkPassword from './utils/checkPassword';
+
+function fetchMock(input, init) {
+  if (input === 'https://api.tokopedia.example.com/login') {
+    const requestBody = JSON.parse(init.body);
+    const result = checkPassword(requestBody.email, requestBody.password);
+    const errorMessage = result ? '' : 'Email and password do not match';
+    const responseData = {
+      success: result,
+      errorMessage,
+    };
+
+    const jsonFn = () => {
+      return new Promise((resolve) => {
+        resolve(responseData);
+      });
+    };
+
+    return new Promise((resolve) => {
+      resolve({ json: jsonFn });
+    });
+  } else {
+    return new Promise((resolve, reject) => {
+      reject(new Error(`"${input}" is not mocked in this test`));
+    });
+  }
+}
 
 describe('Login', () => {
   // describe() bawaan dari javascript buat test
   test(`works fine if email and password match`, async () => {
+    const fetchSpy = jest.spyOn(window, 'fetch').mockImplementation(fetchMock);
     render(<Login />);
 
     const emailField = await screen.findByLabelText('Email');
